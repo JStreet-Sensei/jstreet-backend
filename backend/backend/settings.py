9032c6448febfcad7221e8be696d21d47511bf69
joaoAraki27 <gamerrealms765@gmail.com>
2024-08-29 23:58:04 +0900

    Migrations file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders', # Add corsheaders to handle cross-origin requests
-    'rest_framework',  #  Enables Django Rest Framework
+    'rest_framework',  #  Enables Django Rest Framework 
+    'backend'
 ]
 
 MIDDLEWARE = [
