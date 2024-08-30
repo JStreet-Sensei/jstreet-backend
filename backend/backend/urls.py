32a3ba5267d6d6e40a27a271cbbe8e40483bb0f4
Filippo Quattrocchi <filippo.quattrocchi23@outlook.it>
2024-08-30 13:58:53 +0900

    Initial setup for backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('test/', views.send_test_data), # Link the view to a URL
 ]
