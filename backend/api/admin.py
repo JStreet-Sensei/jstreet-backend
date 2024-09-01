845b666621b7b6956c6e649aed7f6beca800d251
SJSJC <flyflyinu@gmail.com>
2024-09-02 07:29:09 +0900

    add backend urls, models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-
-from .models import User
+from .models import User, Score, Lobby, GameName, WordsLearned, Content
 
 admin.site.register(User)
+admin.site.register(Score)
+admin.site.register(Lobby)
+admin.site.register(GameName)
+admin.site.register(WordsLearned)
+admin.site.register(Content)
