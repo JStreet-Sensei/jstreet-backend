1e277b51d5caf791e216143128ba7a62cf4c25ba
Filippo Quattrocchi <filippo.quattrocchi23@outlook.it>
2024-09-01 11:36:05 +0900

    Fix installed apps setting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders', # Add corsheaders to handle cross-origin requests
     'rest_framework',  #  Enables Django Rest Framework 
-    'backend'
+    'backend',
     'api',
 ]
 
