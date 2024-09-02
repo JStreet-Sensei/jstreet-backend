f8972b120bba291f2b067e31ba584c6c7df7b338
SJSJC <flyflyinu@gmail.com>
2024-09-02 10:29:24 +0900

    Fix model migration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -140,3 +140,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom auth Usermodel
+AUTH_USER_MODEL = "api.User"
