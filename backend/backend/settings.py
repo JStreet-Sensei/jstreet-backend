9ac69f1e45dce342437d1a3ab135283d252515a0
Filippo Quattrocchi <filippo.quattrocchi23@outlook.it>
2024-09-02 22:53:37 +0900

    Allow cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -67,7 +67,8 @@ MIDDLEWARE = [
 ]
 
 # Cors settings
-
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [       # Allows access from the frontend
     os.getenv('FRONTEND_URL','http://localhost:3000')
 ]
