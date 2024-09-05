5bd653d61a02a1161400aa56e95c0b52f7e95e68
Eiichi Sayama <eiichimouse@outlook.jp>
2024-09-05 16:22:00 +0900

    fix:Modify initial data.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import routers
-from . import views
+# from . import views
 
 router = routers.DefaultRouter()
 
