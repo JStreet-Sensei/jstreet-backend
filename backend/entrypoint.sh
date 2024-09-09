3f5f0f2d844fd62cd42548b922d493c59d04698a
Filippo Quattrocchi <filippo.quattrocchi23@outlook.it>
2024-09-09 18:19:47 +0900

    Fix: Fixtures and entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,11 +5,14 @@
 # Perform database migration
 python manage.py migrate
 
-# Create default super user
-python manage.py createsuperuser --noinput
-
 # Collect static files
 python manage.py collectstatic --noinput
+
+# Seed data
+python manage.py loaddata api/fixtures/initial_data.json
+
+# Create default super user
+python manage.py createsuperuser --noinput
 
 # Start Django application with Gunicorn
 gunicorn backend.wsgi:application --bind 0.0.0.0:8000
