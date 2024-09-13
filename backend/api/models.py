b20773ca5f41496874a3f9f14592c90ccbf8e890
Filippo Quattrocchi <filippo.quattrocchi23@outlook.it>
2024-09-13 14:37:03 +0900

    Add date to word learned

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,39 +1,58 @@
+from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Score(models.Model):
-    player1 = models.ForeignKey(User, to_field='id', on_delete=models.CASCADE, related_name="player_1", default=1)
-    player2 = models.ForeignKey(User, to_field='id', on_delete=models.CASCADE, related_name="player_2", default=1)
-    game_id = models.ForeignKey('lobby',to_field='game_id', on_delete=models.CASCADE)
+    player1 = models.ForeignKey(
+        User,
+        to_field="id",
+        on_delete=models.CASCADE,
+        related_name="player_1",
+        default=1,
+    )
+    player2 = models.ForeignKey(
+        User,
+        to_field="id",
+        on_delete=models.CASCADE,
+        related_name="player_2",
+        default=1,
+    )
+    game_id = models.ForeignKey("lobby", to_field="game_id", on_delete=models.CASCADE)
     score = models.IntegerField()
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.player1.username} vs {self.player2.username} - {self.score}'
+        return f"{self.player1.username} vs {self.player2.username} - {self.score}"
+
 
 class Lobby(models.Model):
     game_id = models.AutoField(primary_key=True)
-    owner = models.ForeignKey(User, related_name='lobby_owner', on_delete=models.CASCADE)
-    name = models.CharField(max_length=255, default='Lobby_default_name')
-    game_type = models.ForeignKey('GameName', on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, related_name="lobby_owner", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, default="Lobby_default_name")
+    game_type = models.ForeignKey("GameName", on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'Lobby for game {self.game_id} owned by {self.owner.username}'
+        return f"Lobby for game {self.game_id} owned by {self.owner.username}"
+
 
 class GameName(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100, unique=True)
-    
+
     def __str__(self):
         return self.name
 
+
 class WordsLearned(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.ForeignKey('Content', on_delete=models.CASCADE)
+    content = models.ForeignKey("Content", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.user.username} learned {self.content.japanese_slang}'
+        return f"{self.user.username} learned {self.content.japanese_slang}"
+
 
 class Content(models.Model):
     content_id = models.AutoField(primary_key=True)
